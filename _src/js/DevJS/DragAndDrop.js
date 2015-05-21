5a97b2e00e03b7414e5e1b0dd0982efcaf073d94
IsArlekin <diablsky@gmail.com>
2015-05-22 00:47:21 +0300

    End DraggSlider

--- _src/js/DevJS/DragAndDrop.js
+++ _src/js/DevJS/DragAndDrop.js
@@ -1,17 +1,35 @@
 DragAndDrop = (function(){
     var init = function (){
         var $draggable = $('.draggabilly').draggabilly({
-            // Значит, что область, по которой мы можем передвигать элемент ограниченна контейнером.
+            // Значит, что область, по которой мы можем передвигать элемент
+            // ограниченна контейнером.
             containment: true
-        });
-        var draggie = $draggable.data('draggabilly');
+            }),
+            $draggSlider = $('.dragg-slider__slide').draggabilly({
+                containment: true,
+                // Передвижение только в п пределах ОХ
+                axis: 'x'
+            });
         // Метод срабатывающий при старте, рассчитывает позицию левого верхнего угла,
         // вывожу для примера.
-        $draggable.on( 'dragMove', function() {
+        $draggable.on( 'dragMove', _getPozition);
+        // Изменение opacity у картинки
+        $draggSlider.on( 'dragMove', _setOpacity);
+        },
+        _getPozition = function(){
+            var draggie = $(this).data('draggabilly');
             $('.dragg-window__x').text(draggie.position.x);
             $('.dragg-window__y').text(draggie.position.y);
-        });
-    };
+        },
+        _setOpacity = function() {
+            var draggieSlide = $(this).data('draggabilly'),
+                pozitionDraggSlide = draggieSlide.position.x,
+                fullWidth = $(this).parent().width(),
+                valueOpacity = pozitionDraggSlide/fullWidth;
+
+            $('.dragg-slider__window').text(draggieSlide.position.x);
+            $('.draggabilly__img').css('opacity',valueOpacity)
+        };
     return {
         init: init
     };
--- _src/js/app.js
+++ _src/js/app.js
@@ -1,20 +1,40 @@
 DragAndDrop = (function(){
-    var init = function () {
+    var init = function (){
         var $draggable = $('.draggabilly').draggabilly({
+            // Значит, что область, по которой мы можем передвигать элемент
+            // ограниченна контейнером.
             containment: true
-        });
-        var draggie = $draggable.data('draggabilly');
-        $draggable.on( 'dragMove', function() {
+            }),
+            $draggSlider = $('.dragg-slider__slide').draggabilly({
+                containment: true,
+                // Передвижение только в п пределах ОХ
+                axis: 'x'
+            });
+        // Метод срабатывающий при старте, рассчитывает позицию левого верхнего угла,
+        // вывожу для примера.
+        $draggable.on( 'dragMove', _getPozition);
+        // Изменение opacity у картинки
+        $draggSlider.on( 'dragMove', _setOpacity);
+        },
+        _getPozition = function(){
+            var draggie = $(this).data('draggabilly');
             $('.dragg-window__x').text(draggie.position.x);
             $('.dragg-window__y').text(draggie.position.y);
-        });
+        },
+        _setOpacity = function() {
+            var draggieSlide = $(this).data('draggabilly'),
+                pozitionDraggSlide = draggieSlide.position.x,
+                fullWidth = $(this).parent().width(),
+                valueOpacity = pozitionDraggSlide/fullWidth;
 
-    };
+            $('.dragg-slider__window').text(draggieSlide.position.x);
+            $('.draggabilly__img').css('opacity',valueOpacity)
+        };
     return {
         init: init
     };
 })();
 
-$(document).ready( function() {
+;$(document).ready( function() {
     DragAndDrop.init();
 });
