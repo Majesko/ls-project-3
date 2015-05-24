900abbf17071d05b599ca03a8c82c2590415784d
kukgalina <ya.mail.gk@yandex.ru>
2015-05-24 22:20:43 +0300

    style form

--- _src/js/modules/myjqueryui.js
+++ _src/js/modules/myjqueryui.js
@@ -1,7 +1,7 @@
 $(function() {
 
 // подключение переключателей значений
-	var spinner = $('#x, #y').spinner({
+	var spinner = $('#x, #y, #up-down, #left-right').spinner({
 		min: 0,
 		max: 1000,
 		step: 1
