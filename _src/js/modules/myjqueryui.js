0abbf189acf4e7620e1da8e6a94efc0af3a7813b
kukgalina <ya.mail.gk@yandex.ru>
2015-05-26 20:21:10 +0300

    поправки

--- _src/js/modules/myjqueryui.js
+++ _src/js/modules/myjqueryui.js
@@ -7,14 +7,4 @@ $(function() {
 		step: 1
 
 	});
-
-// подключение ползунка 
-	$('.sidebar__opacity-slider').slider({
-		animate: true,
-		range: "min",
-		value: 50,
-		min: 1,
-		max: 100,
-		step: 1
-	});
 });
